8952398632780f0d42ce68c18313e216a03b8458
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2024-03-18 20:17:53 +0100

    lc88

---
+++ lc88_Merge_Sorted_Array.cpp
@@ -0,0 +1,22 @@
+/*
+ * 88. Merge Sorted Array
+ */
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1;
+        // Idea: i,j two pointers are scanned upside down and wants the bigger one
+        // Detailed judgment: i,j can not cross the line (one < 0, to be another)
+        for (int k = m + n - 1; k >= 0; k--){
+            if (j < 0 || (i >= 0 && nums1[i] >= nums2[j])) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+        }
+    }
+};
+
