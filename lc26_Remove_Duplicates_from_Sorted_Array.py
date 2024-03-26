03a0d2c545cb0a6a14a5dfdf514049f570131982
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2024-03-26 18:00:33 +0100

    lc26

---
+++ lc26_Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,11 @@
+"""
+26. Remove Duplicates from Sorted Array
+"""
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = 0
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i - 1]:
+                nums[n] = nums[i]
+                n += 1
+        return n
